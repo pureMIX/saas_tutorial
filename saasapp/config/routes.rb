a58947462b23ee94c34529b7c96b55c455ccd6a0
Jeff Lucanas <jfylucanas@gmail.com>
2017-09-14 10:18:26 +0000

    Added Plans Table

--- saasapp/config/routes.rb
+++ saasapp/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  #get 'login', to: 'devise/sessions#new', as: 'new_user_session'
+  #get 'sign-up', to: 'devise/registrations#new', as: 'new_user_registration'
 end
