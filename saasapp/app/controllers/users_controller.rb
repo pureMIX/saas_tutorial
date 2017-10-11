1709f3749043478811ff9f1df4a6673f1efe1580
Jeff Lucanas <jfylucanas@gmail.com>
2017-10-11 09:08:32 +0000

    Added more navlinks

--- saasapp/app/controllers/users_controller.rb
+++ saasapp/app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  # Get to /users
+  def index
+    # Get all users
+    @users = User.includes(:profile)
+  end
+  
   # GET to /users/:id
   def show
     @user = User.find(params[:id])
   end
+  
 end
